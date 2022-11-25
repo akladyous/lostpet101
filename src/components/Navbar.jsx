5d7a8997522a41c4155788b64470919ca744e420
Boula Akladyous <akladyous@gmail.com>
2022-11-25 14:47:03 -0500

    updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
-import logo from '../assets/images/icons/logo.png'
 import React from 'react'
+import '../assets/css/navbar.css'
+import logo from '../assets/images/icons/logo.png'
 
 export default function Navbar() {
     const currentUser = null;
     const userStatus = [true, false]
     return (
         <header>
-            <nav className="navbar navbar-expand-lg bg-secondary h-100">
+            <nav className="navbar navbar-expand-lg h-100">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="/">
                         <img src={logo} alt="logo" className='logo' />
@@ -16,26 +17,26 @@ export default function Navbar() {
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="mainNavbar">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0 mx-auto gap-3">
+                        <ul className="navbar-nav me-auto mb-1 mb-lg-0 mx-auto gap-1">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                                <a className="nav-link ps-2 active" aria-current="page" href="/">Home</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/">Link</a>
+                                <a className="nav-link ps-2" href="/">Link</a>
                             </li>
                             <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <a className="nav-link ps-2 dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Dropdown
                                 </a>
                                 <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item" href="/">Action</a></li>
-                                    <li><a className="dropdown-item" href="/">Another action</a></li>
-                                    <li><hr className="dropdown-divider" /></li>
-                                    <li><a className="dropdown-item" href="/">Something else here</a></li>
+                                    <li><a className="dropdown-item " href="/">Action</a></li>
+                                    <li><a className="dropdown-item " href="/">Another action</a></li>
+                                    <li><hr className="dropdown-divider " /></li>
+                                    <li><a className="dropdown-item " href="/">Something else here</a></li>
                                 </ul>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link disabled">Disabled</a>
+                                <a className="nav-link ps-2" href='/'>Disabled</a>
                             </li>
                         </ul>
                         <div className='navbar-nav align-content-center justify-content-center gap-2'>
