fd70b32a38798440c6ee5e3f89b888fcf8571baf
Boula Akladyous <akladyous@gmail.com>
2022-11-29 14:38:41 -0500

    slide-area

--- src/components/ui/Home.jsx
+++ src/components/ui/Home.jsx
@@ -1,44 +1,13 @@
-import Navbar from './Navbar.jsx'
-import '../../assets/css/slide-area.css'
-import dog from '../../assets/images/banner/dog.png'
+import Navbar from './home/Navbar.jsx'
+import SlideArea from './home/SlideArea.jsx'
+import ServiceArea from './home/ServiceArea.jsx'
 
 export default function Home() {
     return (
         <div className="home-page vh-100 vw-100">
             <Navbar />
-            <div className="slide-area">
-                <div className="slide-bg">
-                    <div className="row h-100">
-                        <div className="col-sm-12 col-md-4">
-                            <div className="h-100 d-flex flex-column justify-content-center gap-md-3">
-                                <div className='slide-text text-center'>
-                                    <h4 className="text-light display-1 fw-bold">
-                                        We Care
-                                    </h4>
-                                    <h2 className="text-light fs-1 fw-semibold">
-                                        Your Pets
-                                    </h2>
-                                </div>
-                                <div className='text-center'>
-                                    <p className="text-white px-3">
-                                        Create a free lost or found pet listing that
-                                        is emailed faxed to over 25 local shelters,
-                                        vets and rescue groups.
-                                    </p>
-                                </div>
-                                <div className='text-center my-2'>
-                                    <a href="contact.html" class="btn-boxed">Contact Us</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="d-none d-md-block col-md-8">
-                            <div className='dog-area h-100'>
-                                <img src={dog} alt="dog" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <SlideArea />
+            <ServiceArea />
         </div>
     )
 }
---
+++ src/components/ui/home/ServiceArea.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function ServiceArea() {
+    return (
+        <div className='container'>
+            <div className="row justify-content-center">
+                <div class="col-lg-7 col-md-10">
+                    <div class="section_title text-center mb-95">
+                        <h3>Services for every dog</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="row justify-content-center">
+                <div className="col-md-6">
+
+                </div>
+            </div>
+        </div>
+    )
+}
