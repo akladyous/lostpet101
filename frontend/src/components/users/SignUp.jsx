59b70c5851ff1c0eca699e3d3d0960ef79679135
Boula Akladyous <akladyous@gmail.com>
2022-12-01 16:39:37 -0500

    formBbuilder props

--- frontend/src/components/users/SignUp.jsx
+++ frontend/src/components/users/SignUp.jsx
@@ -3,29 +3,6 @@ import PropTypes from 'prop-types'
 import { MemoizedComponent } from '../../lib/Element.jsx'
 const camelCaseToString = str => str.split(/(?=[A-Z])/).join(' ')
 
-function formData(modelName, columnsName, url, handler) {
-    this.modelName = modelName
-    this.columnsName = columnsName
-    this.url = url
-    this.handler = handler
-}
-formData.prototype.get = function () { }
-var prop = ''
-const userData = {
-    model: 'user',
-    url: 'users/signup',
-    columns: {
-        email: {
-            attributes: { type: 'text', name: 'email', className: 'form-control', id: 'email', value: '' },
-            handler: function () { },
-        }
-    },
-
-    get [prop]() {
-        this.columns.forEach(col => { })
-    }
-}
-
 function initialState(initialValue) {
     return { ...initialValue }
 }
