495135dfef7016cd62c345bfd56fe8139589685f
Boula Akladyous <akladyous@gmail.com>
2022-12-03 00:45:22 -0500

    seed

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -4,10 +4,10 @@ class UserSerializer < ActiveModel::Serializer
   def avatar
         # if object.is_attached?
         if object.avatar.attached?
-            # Rails.application.routes.url_helpers.rails_blob_url(self.object.avatar.blob, only_path: true)
-            object.avatar.blob.url
+            Rails.application.routes.url_helpers.rails_blob_url(self.object.avatar.blob, only_path: true)
         else
             nil
         end
     end
 end
+Rails.application.routes.url_helpers.rails_blob_url(u1.avatar.blob, only_path: true)
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'ruby-progressbar'
+require 'faker'
+Faker::Config.locale = :en
+
+system('clear')
+puts 'How many records ?'
+USERS_TO_CREATE = gets.strip.to_i
+system('clear')
+puts "🌱 Seeding #{USERS_TO_CREATE.to_i} Users ..."
+
+user_progress = ProgressBar.create(title: 'Creating users', total: USERS_TO_CREATE)
+
+folder = Rails.root.join('app/assets/images/avatars')
+images = proc { |avatars_folder|
+  Dir.entries(avatars_folder).map(&:downcase).reject { |folder| folder.starts_with? '.' }
+}
+
+1.upto(USERS_TO_CREATE) do |idx|
+  User.create do |user|
+    email = Faker::Internet.unique.email
+    user.email = email
+    user.password = '000000'
+    user.password_confirmation = '000000'
+    user.avatar.attach(io: File.open("app/assets/images/avatars/#{idx}.jpg"), filename: "#{idx}.jpg")
+    user.save
+    user_progress.increment
+  end
+end
