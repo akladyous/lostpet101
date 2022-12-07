cf46cb800092085da405e487f01c3eb4b4495979
Boula Akladyous <akladyous@gmail.com>
2022-12-07 17:39:31 -0500

    completed team-area component

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
             backgroundImage: {
                 'header-image': "url('../images/banner/bradcam.png')",
                 'slide-area': "url('../images/banner/banner.png')",
-                'service-icon': "url('../images/service/service_icon_bg_1.png')"
+                'team-area': "url('../images/banner/contact_bg.png')",
+                'service-icon': "url('../images/service/service_icon_bg_1.png')",
             }
         },
     },
