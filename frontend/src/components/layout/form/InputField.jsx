0098cdf70597127fc95704f9590c3ce5b7cca83f
Boula Akladyous <akladyous@gmail.com>
2022-12-08 09:09:48 -0500

    updated input-field component

--- frontend/src/components/layout/form/InputField.jsx
+++ frontend/src/components/layout/form/InputField.jsx
@@ -1,31 +1,29 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-
 const formClasses = {
     label: 'mb-2 block text-sm font-medium text-gray-700 capitalize',
-    textField: 'block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-orange-300 focus:outline-none focus:ring-orange-500 sm:text-sm'
-
-}
+    textField:
+        'block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-orange-300 focus:outline-none focus:ring-orange-500 sm:text-sm',
+};
 
 export const InputField = (props) => {
     // console.log(props.input.name, ' : ', props)
-    const state = useSelector(state => state.users)
-    const { input, label, value, onChange } = props || {}
+    const state = useSelector((state) => state.users);
+    const { input, label, value, onChange } = props || {};
     React.useEffect(() => {
-        console.log('useEffect : ', input.name)
-    }, [])
+        console.log('useEffect : ', input.name);
+    }, []);
     return (
         <div>
-            {
-                label
-                    ? <label
-                        htmlFor={label?.htmlFor ?? input.name}
-                        className={formClasses.label}>
-                        {label?.name}
-                    </label>
-                    : null
-            }
+            {label ? (
+                <label
+                    htmlFor={label?.htmlFor ?? input.name}
+                    className={formClasses.label}
+                >
+                    {label?.name}
+                </label>
+            ) : null}
             <input
                 {...input}
                 id={input.name}
@@ -33,19 +31,14 @@ export const InputField = (props) => {
                 value={value}
                 onChange={onChange}
             />
-            {
-
-                state.error[input?.name]
-                    ?
-                    <div className='pt-2'>
-                        <p className='text-red-600 text-sm'>
-                            {`${input?.name} ${state.error[input?.name][0]}`}
-                        </p>
-                    </div>
-                    : null
-
-            }
-        </div >
-    )
+            {state.error[input?.name] ? (
+                <div className="pt-2">
+                    <p className="text-sm text-red-600">
+                        {`${input?.name} ${state.error[input?.name][0]}`}
+                    </p>
+                </div>
+            ) : null}
+        </div>
+    );
 };
-export const MemoizedComponent = React.memo(InputField)
+export const MemoizedComponent = React.memo(InputField);
