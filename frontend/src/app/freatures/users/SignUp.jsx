92ba56171e81a317a82fd2fc73a6a95e94d8e1b4
Boula Akladyous <akladyous@gmail.com>
2022-12-11 13:50:10 -0500

    update inputField component

--- frontend/src/app/freatures/users/SignUp.jsx
+++ frontend/src/app/freatures/users/SignUp.jsx
@@ -1,7 +1,7 @@
 import { Link, useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { useCallback, useRef } from 'react';
-import { Formik } from 'formik';
+import React, { useCallback, useRef } from 'react';
+import { Formik, ErrorMessage } from 'formik';
 import { InputField } from './form/InputField.jsx';
 import { signUpSchema } from './form/signUpSchema.js';
 import { FormMessages } from './form/FormMessages.jsx';
@@ -74,82 +74,119 @@ export default function SignUp() {
                             initialValues={formInitialState}
                             onSubmit={handleSubmit}
                             validationSchema={formConstrains}
+                            validateOnChange={false}
                         >
-                            {(props) => (
-                                <form
-                                    className="space-y-6"
-                                    onSubmit={props.handleSubmit}
-                                >
-                                    {formFields.map((obj, idx) => {
-                                        return (
-                                            <InputField
-                                                key={idx}
-                                                InputField={obj}
-                                                classes={formClasses}
-                                                {...props}
-                                            />
-                                        );
-                                    })}
-                                    <FormMessages ref={formMessageRef} />
-                                    <div className="flex items-center justify-between">
-                                        <div className="flex items-center">
-                                            <input
-                                                id="remember-me"
-                                                name="remember-me"
-                                                type="checkbox"
-                                                className="h-4 w-4 rounded border-gray-300 text-orange-600 focus:ring-orange-500"
-                                                disabled
-                                            />
-                                            <label
-                                                htmlFor="remember-me"
-                                                className="ml-2 block text-sm text-gray-900"
-                                            >
-                                                Remember me
-                                            </label>
-                                        </div>
-
-                                        <div className="text-sm">
-                                            <Link
-                                                to="/users/signin"
-                                                state={'User SignUp'}
-                                                className="font-medium text-orange-600 hover:text-orange-500"
-                                            >
-                                                have account already?
-                                            </Link>
-                                        </div>
-                                    </div>
-
-                                    <div>
-                                        <button
-                                            type="submit"
-                                            disabled={props.isSubmitting}
-                                            className="flex w-full justify-center rounded-md border border-transparent bg-orange-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
-                                        >
-                                            {props.isSubmitting ? (
-                                                <svg
-                                                    role="status"
-                                                    className="mr-3 inline h-4 w-4 animate-spin text-white"
-                                                    viewBox="0 0 100 101"
-                                                    fill="none"
-                                                    xmlns="http://www.w3.org/2000/svg"
+                            {(_formikProps) => {
+                                return (
+                                    <form
+                                        name="signup"
+                                        className="space-y-6"
+                                        onSubmit={_formikProps.handleSubmit}
+                                        onReset={_formikProps.handleReset}
+                                    >
+                                        {formFields.map((field, idx) => {
+                                            return (
+                                                <div key={idx}>
+                                                    <InputField
+                                                        fieldName={
+                                                            field.input.name
+                                                        }
+                                                        fieldType={
+                                                            field.input.type
+                                                        }
+                                                        fieldClasses={
+                                                            formClasses.input
+                                                        }
+                                                        label={field.label.name}
+                                                        labelClasses={
+                                                            formClasses.label
+                                                        }
+                                                        handleChange={
+                                                            _formikProps.handleChange
+                                                        }
+                                                        handleBlur={
+                                                            _formikProps.handleBlur
+                                                        }
+                                                        value={
+                                                            _formikProps.values[
+                                                                field.input.name
+                                                            ]
+                                                        }
+                                                    >
+                                                        <ErrorMessage
+                                                            name={
+                                                                field.input.name
+                                                            }
+                                                            component={'p'}
+                                                            className="pt-2 text-sm text-red-600"
+                                                        />
+                                                    </InputField>
+                                                </div>
+                                            );
+                                        })}
+                                        <FormMessages ref={formMessageRef} />
+                                        <div className="flex items-center justify-between">
+                                            <div className="flex items-center">
+                                                <input
+                                                    id="remember-me"
+                                                    name="remember-me"
+                                                    type="checkbox"
+                                                    className="h-4 w-4 rounded border-gray-300 text-orange-600 focus:ring-orange-500"
+                                                    disabled
+                                                />
+                                                <label
+                                                    htmlFor="remember-me"
+                                                    className="ml-2 block text-sm text-gray-900"
                                                 >
-                                                    <path
-                                                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                                                        fill="#E5E7EB"
-                                                    />
-                                                    <path
-                                                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                                                        fill="currentColor"
-                                                    />
-                                                </svg>
-                                            ) : null}
-                                            {props.isSubmitting
-                                                ? 'processing..'
-                                                : 'Submit'}
-                                        </button>
-                                    </div>
-                                </form>
-                            )}
+                                                    Remember me
+                                                </label>
+                                            </div>
+
+                                            <div className="text-sm">
+                                                <Link
+                                                    to="/users/signin"
+                                                    state={'User SignUp'}
+                                                    className="font-medium text-orange-600 hover:text-orange-500"
+                                                >
+                                                    have account already?
+                                                </Link>
+                                            </div>
+                                        </div>
+
+                                        <div>
+                                            <button
+                                                type="submit"
+                                                disabled={
+                                                    _formikProps.isSubmitting
+                                                }
+                                                className="flex w-full justify-center rounded-md border border-transparent bg-orange-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
+                                            >
+                                                {_formikProps.isSubmitting ? (
+                                                    <svg
+                                                        role="status"
+                                                        className="mr-3 inline h-4 w-4 animate-spin text-white"
+                                                        viewBox="0 0 100 101"
+                                                        fill="none"
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                    >
+                                                        <path
+                                                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                                            fill="#E5E7EB"
+                                                        />
+                                                        <path
+                                                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                                            fill="currentColor"
+                                                        />
+                                                    </svg>
+                                                ) : null}
+                                                {_formikProps.isSubmitting
+                                                    ? 'processing..'
+                                                    : 'Submit'}
+                                            </button>
+                                        </div>
+                                    </form>
+                                );
+                            }}
                         </Formik>
 
                         <AuthenticateWithProvider />
--- frontend/src/app/freatures/users/form/signUpSchema.js
+++ frontend/src/app/freatures/users/form/signUpSchema.js
@@ -16,8 +16,7 @@ const formFields = [
 ];
 const formClasses = {
     label: 'mb-2 block text-sm font-medium text-gray-700 capitalize',
-    textField:
-        'block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-orange-300 focus:outline-none focus:ring-orange-500 sm:text-sm',
+    input: 'peer block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-orange-300 focus:outline-none focus:ring-orange-500 sm:text-sm',
 };
 const formConstrains = Yup.object({
     email: Yup.string().email('Invalid email address').required('Required'),
