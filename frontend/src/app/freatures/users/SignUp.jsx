9943493b766d51020006aef3cf6372ad2fe62ac3
Boula Akladyous <akladyous@gmail.com>
2022-12-11 13:54:35 -0500

    update signup form

--- frontend/src/app/freatures/users/SignUp.jsx
+++ frontend/src/app/freatures/users/SignUp.jsx
@@ -7,6 +7,7 @@ import { signUpSchema } from './form/signUpSchema.js';
 import { FormMessages } from './form/FormMessages.jsx';
 import { usersSignUp } from '../../../state/thunks/users/usersSignUp.js';
 import AuthenticateWithProvider from './form/AuthenticateWithProvider.jsx';
+import RememberMe from './form/RememberMe.jsx';
 
 const [formFields, formInitialState, formClasses, formConstrains] =
     signUpSchema();
@@ -126,22 +127,7 @@ export default function SignUp() {
                                         })}
                                         <FormMessages ref={formMessageRef} />
                                         <div className="flex items-center justify-between">
-                                            <div className="flex items-center">
-                                                <input
-                                                    id="remember-me"
-                                                    name="remember-me"
-                                                    type="checkbox"
-                                                    className="h-4 w-4 rounded border-gray-300 text-orange-600 focus:ring-orange-500"
-                                                    disabled
-                                                />
-                                                <label
-                                                    htmlFor="remember-me"
-                                                    className="ml-2 block text-sm text-gray-900"
-                                                >
-                                                    Remember me
-                                                </label>
-                                            </div>
-
+                                            <RememberMe />
                                             <div className="text-sm">
                                                 <Link
                                                     to="/users/signin"
