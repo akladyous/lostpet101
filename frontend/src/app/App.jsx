f428bfc2d87e63c049c79cc5c46230cf11133d27
Boula Akladyous <akladyous@gmail.com>
2022-12-24 00:43:41 -0500

    add app routes

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -11,28 +11,27 @@ import Listings from "./freatures/listings/Listings.jsx";
 import ListingWizard from "./freatures/listings/ListingWizard.jsx";
 
 function App() {
-    return (
-        <div className='main'>
-            <Routes>
-                <Route path='/' element={<Root />}>
-                    <Route path='/' element={<Home />} />
-                    <Route path='about' element={<Home />} />
-                    <Route path='feedback' element={<Home />} />
-                    <Route path='users' element={<Users />}>
-                        <Route index path='signin' element={<SignIn />} />
-                        <Route index path='signout' element={<SignOut />} />
-                        <Route index path='signup' element={<SignUp />} />
-                        <Route index path='profile' element={<Profile />} />
-                    </Route>
-                    <Route path='listings' element={<Listings />}>
-                        <Route index path='new' element={<ListingWizard />} />
-                        {/* <Route index path="report" element={<NewReport />} /> */}
-                        {/* <Route index path="pet" element={<NewPet />} /> */}
-                    </Route>
-                </Route>
-            </Routes>
-        </div>
-    );
+  return (
+    <div className='main'>
+      <Routes>
+        <Route path='/' element={<Root />}>
+          <Route path='/' element={<Home />} />
+          <Route path='about' element={<Home />} />
+          <Route path='feedback' element={<Home />} />
+          <Route path='users' element={<Users />}>
+            <Route index path='signin' element={<SignIn />} />
+            <Route index path='signout' element={<SignOut />} />
+            <Route index path='signup' element={<SignUp />} />
+            <Route index path='profile' element={<Profile />} />
+          </Route>
+          <Route path='listings' element={<Listings />}>
+            <Route index path='new' element={<ListingWizard />} />
+            {/* <Route index path="report" element={<NewReport />} /> */}
+            {/* <Route index path="pet" element={<NewPet />} /> */}
+          </Route>
+        </Route>
+      </Routes>
+    </div>
+  );
 }
-
 export default App;
