645f17bb3109c9eea017330c40cde00a080bfed5
Boula Akladyous <akladyous@gmail.com>
2023-01-12 10:42:22 -0500

    created testimonialsRoot

---
+++ client/src/app/freatures/testmonials/TestmonialRoot.jsx
@@ -0,0 +1,11 @@
+import { Outlet, useParams, useLocation } from 'react-router-dom';
+
+export default function TestmonialRoot() {
+  const location = useLocation();
+  const params = useParams();
+  return (
+    <div className="ReportsRoot w-10/12 md:w-3/4 max-w-7xl mx-auto my-8">
+      <Outlet context={{ params: params, location: location }} />
+    </div>
+  );
+}
