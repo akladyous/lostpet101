1b0c5d1f3948d92e693b872c9e481503f2cde831
Boula Akladyous <akladyous@gmail.com>
2023-01-14 17:05:09 -0500

    added production email configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,11 +55,8 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "petFinder_production"
 
-  config.action_mailer.perform_caching = false
-
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -83,4 +80,22 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.action_mailer.default_url_options = { host: Rails.application.credentials.dig(:email_provider, :host) }
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: Rails.application.credentials.dig(:email_provider, :address),
+    port: Rails.application.credentials.dig(:email_provider, :port),
+    domain: Rails.application.credentials.dig(:email_provider, :domain),
+    user_name: Rails.application.credentials.dig(:email_provider, :user),
+    password: Rails.application.credentials.dig(:email_provider, :password),
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    open_timeout: 5,
+    read_timeout: 5
+  }
 end
