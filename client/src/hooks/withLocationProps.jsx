96391c2fc1c12f4db357abb8ca622ca0e7320ef6
Boula Akladyous <akladyous@gmail.com>
2023-01-15 14:20:48 -0500

    added withLocalProps hook

--- client/src/hooks/withLocationProps.jsx
+++ client/src/hooks/withLocationProps.jsx
@@ -8,8 +8,6 @@ export function withLocationProps(WrappedComponent) {
 
     const { state } = useLocation();
     const { name } = useParams();
-    // console.log('useLocation(); : ', useLocation());
-    // console.log('useParams(); : ', useParams());
 
     return (
       <WrappedComponent
