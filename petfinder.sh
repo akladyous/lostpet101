b0e0a6fe7b0e03a2583a3b61725e49491598fe97
Boula Akladyous <akladyous@gmail.com>
2023-01-16 15:56:39 -0500

    added docker-compose build

--- petfinder.sh
+++ petfinder.sh
@@ -5,6 +5,5 @@
 docker stop $(docker ps -a -q)
 docker rmi -f $(docker images -aq)
 docker system prune --force
-docker compose up
 docker volume create postgres_data
 docker compose build & docker compose up
