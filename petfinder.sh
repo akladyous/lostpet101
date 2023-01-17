09d1d6a6958bb674f4757d22de776370dbb001fb
Boula Akladyous <akladyous@gmail.com>
2023-01-17 13:39:41 -0500

    added seeding process do petfinder main script

--- petfinder.sh
+++ petfinder.sh
@@ -7,3 +7,4 @@ docker rmi -f $(docker images -aq)
 docker system prune --force
 docker volume create postgres_data
 docker compose build & docker compose up -d
+docker exec -it petfinder rails db:seed
