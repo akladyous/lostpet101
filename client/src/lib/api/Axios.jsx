f3847a1d5164efc656781cf199f1c383732159ee
Boula Akladyous <akladyous@gmail.com>
2023-01-18 01:16:12 -0500

    removed port 3000 from axios / redux query

--- client/src/lib/api/Axios.jsx
+++ client/src/lib/api/Axios.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const Axios = axios.create({
   headers: { 'Content-Type': 'application/json' },
-  baseURL: 'http://localhost:3000',
+  baseURL: 'http://localhost',
   withCredentials: true,
   // Accept: 'application/json',
   // delayed: true
--- client/src/state/api/api.js
+++ client/src/state/api/api.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'http://localhost:3000',
+  baseUrl: 'http://localhost',
   credentials: 'include',
   headers: { 'Content-Type': 'application/json' },
 });
