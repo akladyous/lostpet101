ba177176eb25c2bfe6df337c2286f6ee7670b2c5
Boula Akladyous <akladyous@gmail.com>
2023-01-19 10:58:30 -0500

    completed dockerfile version 3

--- petfinder.sh
+++ petfinder.sh
@@ -7,6 +7,12 @@ docker rmi -f $(docker images -aq)
 docker system prune --force
 docker volume rm $(docker volume ls -qf dangling=true)
 
+docker ps -a -q --filter="name=pet"
+docker stop $(docker ps -a -q --filter="name=pet")
+
+docker rm $(docker stop $(docker ps -a -q --filter="name=pet")) -f
+docker rmi $(docker images --filter=reference="pet*" -q) -f
+
 docker volume create postgres_data
 docker volume create petfinder_data
 docker compose build
