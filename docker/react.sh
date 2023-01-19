07162cd1ae098659d4ef5325e480bbf88f7c679e
Boula Akladyous <akladyous@gmail.com>
2023-01-19 02:58:30 -0500

    completed docker

--- docker/react.sh
+++ docker/react.sh
@@ -3,4 +3,5 @@ rm -rfv ./public/*
 npm ci --prefix ./client
 npm run build --prefix ./client
 cp -a ./client/build/.  ./public
-rm ./client/build -r
+rm -r ./client/build
+rm -r ./client/node_modules
