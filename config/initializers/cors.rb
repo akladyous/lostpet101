ec79dd96c93eb91f60258aacaf1451f06447385d
Boula Akladyous <akladyous@gmail.com>
2023-01-19 14:07:00 -0500

    cors origins updates

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.action_controller.forgery_protection_origin_check = fal
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3300"
+    origins "127.0.0.1:3000"
 
     resource "*",
       headers: :any,
