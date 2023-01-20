a63e8d6eac1e25ae7cc019fa07b5981ac1b85a90
Boula Akladyous <akladyous@gmail.com>
2023-01-20 12:55:21 -0500

    added Dockerfile.frontend

--- docker/react.sh
+++ docker/react.sh
@@ -1,7 +1,10 @@
 
-rm -rfv ./public/*
-npm ci --prefix ./client
-npm run build --prefix ./client
-cp -a ./client/build/.  ./public
-rm -r ./client/build
-rm -r ./client/node_modules
+# rm -rfv ./public/*
+# npm ci --prefix ./client
+# npm run build --prefix ./client
+# cp -a ./client/build/.  ./public
+# rm -r ./client/build
+# rm -r ./client/node_modules
+
+
+cp -a ./app/build/. ./public
