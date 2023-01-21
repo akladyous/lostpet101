3310e4af8dd108c8510e2c022287cff58120f467
Boula Akladyous <akladyous@gmail.com>
2023-01-21 13:24:44 -0500

    added docker-compose pdocution

--- docker/install.sh
+++ docker/install.sh
@@ -16,11 +16,17 @@ docker rm $(docker stop $(docker ps -a -q --filter="name=pet")) -f # remove cont
 docker rmi $(docker images --filter=reference="pet*" -q) -f # remove image by name
 
 docker volume create postgres_data
-docker volume create petfinder_data
+docker volume create shared_data
+docker volume create gem_cache
 
 
 # RUN POSTGRES ------------------------------------------------------------------
-docker run --env-file .env -d --name postgres -p 5432:5432 postgres:15-alpine
+docker run -d \
+  -v postgres_data:/var/lib/postgresql/data \
+  --env-file .env \
+  --name postgres \
+  -p 5432:5432 \
+  postgres:15-alpine
 # BUILD FRONTEND && COPY TO RAILS PUBLIC FOLDER --------------------------------
 DOCKER_BUILDKIT=1 docker build  --file ./docker/Dockerfile.frontend.prod --tag react --output public .
 # RUN FRONTEND DEVELOPMENT --------------------------------
@@ -32,19 +38,14 @@ docker run -d -it --rm  \
   --name frontend \
   frontend
 
-docker run -it --rm --entrypoint /bin/bash petfinder -c bash
+
+docker build -t petfinder .
+docker build --no-cache -f ./docker/Dockerfile.ver1 --target rails -t petfinder .
+
+docker run -it --rm petfinder bash
 docker run -it --rm --entrypoint /bin/bash petfinder -c bash
 docker run -it --rm --name petfinder petfinder /bin/bash
 
-docker build --no-cache -f ./docker/Dockerfile.ver1 --target rails -t petfinder .
-docker run -it --rm petfinder bash
-
-docker build -t petfinder .
 docker start petfinder
 docker exec -it petfinder rails db:seed
 
-
-
-EDITOR="code --wait" bin/rails credentials:edit
-EDITOR="code --wait" bin/rails credentials:edit --environment production
-EDITOR="code --wait" bin/rails credentials:edit --environment development
