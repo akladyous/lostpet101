833866d2c812297e70c4368bad504732a7c5ecd3
Boula Akladyous <akladyous@gmail.com>
2023-01-22 18:57:24 -0500

    rtk basequery

--- frontend/src/state/api/api.js
+++ frontend/src/state/api/api.js
@@ -3,7 +3,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const baseQuery = fetchBaseQuery({
   baseUrl: 'http://localhost:3000/',
   prepareHeaders(headers) {
-    headers.set("Content-Type", "application/json")
+    headers.set('Content-Type', 'application/json');
+    headers.set('Access-Control-Allow-Origin', '*');
     return headers;
   },
   // credentials: 'include',
