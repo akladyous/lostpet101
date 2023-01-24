0da217c79dda2a3df2029dcf8807895a09af98f9
Boula Akladyous <akladyous@gmail.com>
2023-01-24 16:12:12 -0500

    added image blob url serializer

--- app/serializers/pet_serializer.rb
+++ app/serializers/pet_serializer.rb
@@ -1,7 +1,9 @@
 class PetSerializer < ActiveModel::Serializer
+  # include Rails.application.routes.url_helpers
   attributes :id, :name, :species, :gender, :size, :breed, :color, :age, :collar, :description, :photo_url, :created_at, :updated_at
   def photo_url
     object.image.blob.url if object.image.attached?
+    # rails_blob_path(object.image, only_path: true) if object.image.attached?
   end
   has_many :likes
   # has_one :report
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
+  # include Rails.application.routes.url_helpers
   attributes :email, :photo_url, :last_signin_at, :created_at
 
   def photo_url
     object.avatar.blob.url if object.avatar.attached?
+    # rails_blob_path(object.avatar, only_path: true) if object.avatar.attached?
   end
 end
