b7b040b6f037a86a9888659abb0f2091ca39bbc0
Boula Akladyous <akladyous@gmail.com>
2023-01-23 20:03:19 -0500

    added report controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,5 @@
 class ReportsController < ApplicationController
-  skip_before_action :verify_authenticity_token
   skip_before_action :authenticate_user, only: %i[index search]
-
   skip_before_action :verify_authenticity_token
   before_action :set_report, only: %i[ show update destroy ]
 
