e92f7c560eec1d0035ec20423379701f7e896048
Boula Akladyous <akladyous@gmail.com>
2023-01-24 18:24:02 -0500

    added docker entrypoint script

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -7,7 +7,6 @@ bundle check || bundle install --jobs 20 --retry 5
 rm -f $APP_PATH/tmp/pids/server.pid
 
 bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:create db:migrate
-bundle exec rake db:seed
 
 # bundle exec ${@}
 exec "$@"
