5e5cf1ba29f007463c4e5d9649e1c79dd752db8f
Boula Akladyous <akladyous@gmail.com>
2023-01-24 19:13:03 +0000

    updatged cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,4 +1,4 @@
-Rails.application.config.action_controller.forgery_protection_origin_check = false
+# Rails.application.config.action_controller.forgery_protection_origin_check = false
 
 # Be sure to restart your server when you modify this file.
 
