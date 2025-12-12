6807960cac70b2405384d294ca1b3b5d6afbc961
Boula Akladyous <akladyous@gmail.com>
2025-12-12 11:45:59 -0500

    Untracked files on lostpet101

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -2,11 +2,20 @@
 set -e
 
 echo "Environment: $RAILS_ENV"
+
+# Check and install missing gems
 bundle check || bundle install --jobs 20 --retry 5
 
-rm -f $APP_PATH/tmp/pids/server.pid
+# Remove pre-existing server.pid
+rm -f "$APP_PATH/tmp/pids/server.pid"
 
-bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:create db:migrate
+# Database setup and migrations
+if bundle exec rake db:migrate 2>/dev/null; then
+    echo "Database migrated successfully."
+else
+    echo "Database does not exist. Creating and migrating the database..."
+    bundle exec rake db:create db:migrate
+fi
 
-# bundle exec ${@}
+# Execute the container's main process
 exec "$@"
